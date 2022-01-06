7e6304db9eac76e1f14905f72f298d95ea555b55
Miguel Aguila <maguila.apple@gmail.com>
2022-01-06 18:06:09 +0800

    Completed log in screen layout

--- Swift_Date_Calculator/Swift_Date_Calculator/ViewController.swift
+++ Swift_Date_Calculator/Swift_Date_Calculator/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var logIn: UIButton!
+    @IBOutlet weak var createNewAccount: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+    }
+    @IBAction func emailEdit(_ sender: Any) {
+        if email.text == "email"{
+            email.text = nil;
+            email.textColor = .black
+        }
+    }
+    
+    @IBAction func usernameEdit(_ sender: Any) {
+        if username.text == "Name"{
+            username.text = nil;
+            username.textColor = .black
+        }
+    }
+    @IBAction func passwordEdit(_ sender: Any) {
+        if password.text == "Password"{
+            password.text = nil
+            password.textColor = .black
+            password.isSecureTextEntry = true
+        }
+    }
+    
+    
 
 }
 
